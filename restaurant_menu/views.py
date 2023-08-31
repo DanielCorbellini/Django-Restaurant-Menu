8f8e15b76e48a09f6444c54392c3866fb9dc83c1
Daniel <daniel.corbellini28@gmail.com>
2023-08-31 15:46:19 -0300

    Start the front-end with jinja code

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Item
+from .models import Item, MEAL_TYPE
 
 
 class MenuList(generic.ListView):
@@ -8,8 +8,8 @@ class MenuList(generic.ListView):
     template_name = "index.html"
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = {"meals": ["Pizza", "Pasta"],
-                   "ingredients": ["things"]}
+        context = super().get_context_data(**kwargs)
+        context["meals"] = MEAL_TYPE
         return context
 
 
